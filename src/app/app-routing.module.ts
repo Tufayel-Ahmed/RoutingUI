4676702ca81be81a308a6b7a212e03f8188622ae
Tufayel-Ahmed <tufayelahmed.edu@gmail.com>
2022-12-21 05:52:09 +0600

    Angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponentComponent },
+  { path: 'second-component', component: SecondComponentComponent },
+  {path: '', redirectTo: '/second-component', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
